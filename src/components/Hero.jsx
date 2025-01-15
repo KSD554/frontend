3a293cb95ea36ea8c231e3f59570168e2c635cc9
Sadok <kouassisadok9@gmail.com>
2025-01-15 01:31:20 +0000

    2e commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -18,10 +18,10 @@ const Hero = () => {
           {/* button */}
           <div className="inline-flex items-center justify-center gap-4 p-2 bg-white rounded-xl">
             <div className="text-center regular-14 leading-tight pl-5">
-              <h5 className="uppercase font-bold">10% de réduction</h5>
-              <p className="regular-14">Sur tous les biens</p>
+              <h5 className="uppercase font-bold">-10% sur tout</h5>
+<p className="regular-14">Tous les biens</p>
             </div>
-            <Link to={"/listing"} className="btn-secondary rounded-xl flexCenter !py-5">Achetez maintenant</Link>
+            <Link to={"/listing"} className="btn-secondary rounded-xl flexCenter !py-5">Voir</Link>
           </div>
         </div>
       </div>
