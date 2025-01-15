9d2769630ca74da6f79b5553a8e7ae7e65fe3557
Sadok <kouassisadok9@gmail.com>
2025-01-15 01:33:16 +0000

    3e

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
       <div className="max-padd-container bg-hero bg-center bg-cover bg-no-repeat h-[655px] w-full rounded-3xl">
         <div className="relative top-32 xs:top-52">
           <span className="medium-18">Bienvenue chez CasaCentral</span>
-          <h1 className="h1 capitalize max-w-[40rem]">
+          <h1 className="h1 capitalize max-w-[35rem]">
             Découvrez des maisons exceptionnelles avec CasaCentral
           </h1>{" "}
           <p className="my-10 max-w-[33rem]">
@@ -18,7 +18,7 @@ const Hero = () => {
           {/* button */}
           <div className="inline-flex items-center justify-center gap-4 p-2 bg-white rounded-xl">
             <div className="text-center regular-14 leading-tight pl-5">
-              <h5 className="uppercase font-bold">-10% sur tout</h5>
+              <h5 className="uppercase font-bold">-10% sur</h5>
 <p className="regular-14">Tous les biens</p>
             </div>
             <Link to={"/listing"} className="btn-secondary rounded-xl flexCenter !py-5">Voir</Link>
