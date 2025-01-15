ac0edc43d91db2355141c256e226d89b3f8db6cc
Sadok <kouassisadok9@gmail.com>
2025-01-15 01:38:45 +0000

    4e

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,9 +6,9 @@ const Hero = () => {
       <div className="max-padd-container bg-hero bg-center bg-cover bg-no-repeat h-[655px] w-full rounded-3xl">
         <div className="relative top-32 xs:top-52">
           <span className="medium-18">Bienvenue chez CasaCentral</span>
-          <h1 className="h1 capitalize max-w-[35rem]">
+          <h3 className="h1 capitalize max-w-[35rem]">
             Découvrez des maisons exceptionnelles avec CasaCentral
-          </h1>
+          </h3>
           <p className="my-10 max-w-[33rem]">
             Nous proposons des maisons uniques alliant confort, design moderne
             et qualité. Avec CasaCentral, trouvez la propriété idéale, choisie
