382d53f4e5082eaf6d38791e7789a4f363aa16e1
Sadok <kouassisadok9@gmail.com>
2025-01-15 01:36:25 +0000

    bb

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,20 +8,24 @@ const Hero = () => {
           <span className="medium-18">Bienvenue chez CasaCentral</span>
           <h1 className="h1 capitalize max-w-[35rem]">
             Découvrez des maisons exceptionnelles avec CasaCentral
-          </h1>{" "}
+          </h1>
           <p className="my-10 max-w-[33rem]">
-            Nous vous offrons une sélection de maisons uniques, alliant confort,
-            design moderne et qualité supérieure. Trouvez la maison de vos rêves
-            avec CasaCentral, où chaque propriété est choisie avec soin pour
-            répondre à vos besoins et attentes.
+            Nous proposons des maisons uniques alliant confort, design moderne
+            et qualité. Avec CasaCentral, trouvez la propriété idéale, choisie
+            avec soin pour vous.
           </p>
           {/* button */}
           <div className="inline-flex items-center justify-center gap-4 p-2 bg-white rounded-xl">
             <div className="text-center regular-14 leading-tight pl-5">
               <h5 className="uppercase font-bold">-10% sur</h5>
-<p className="regular-14">Tous les biens</p>
+              <p className="regular-14">Tous les biens</p>
             </div>
-            <Link to={"/listing"} className="btn-secondary rounded-xl flexCenter !py-5">Voir</Link>
+            <Link
+              to={"/listing"}
+              className="btn-secondary rounded-xl flexCenter !py-5"
+            >
+              Voir maintenant
+            </Link>
           </div>
         </div>
       </div>
